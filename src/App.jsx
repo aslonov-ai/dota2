720a7fe196af2864c095d64570271de69095b4dc
rrt78537-dotcom <rrt78537@gmail.com>
2026-03-06 16:02:33 +0500

    Appjs add

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,27 @@
-
 import Users from "./pages/Users";
 import Main from "./pages/Main";
 import { Routes, Route, Link } from "react-router-dom";
 import Heros from "./pages/Heros"
 import UserInfos from "./pages/UserInfos"
 import Live from "./pages/Live"
-// import Users from "./pages/Users"
-// import Main from "./pages/Main"
-// import Users from "./pages/Users"
-import axios from "axios"
-
-
+import Teams from "./pages/Teams";
+import Matches from "./pages/Matches";
+import Leagues from "./pages/Leagues";
 
 function App() {
   return (
     <Routes>
-
       <Route path="/" element={<Main />} />
       <Route path="/users" element={<Users />} />
       <Route path="/main" element={<Main />} />
-      <Route path="/heroes" element={<Heros />} />
+      <Route path="/heros" element={<Heros />} />
       <Route path="/userinfos" element={<UserInfos />} />
       <Route path="/live" element={<Live />} />
+      <Route path="/teams" element={<Teams />} />
+      <Route path="/matches" element={<Matches />} />
+      <Route path="/leagues" element={<Leagues />} />
     </Routes>
   );
-  // <Routes>
-  //   <Route path='/users' element={<Users/>}/>
-  // </Routes>
-
-  // <div>
-  //   <Heros />
-  //   <Routes>
-  //     <Route path='/' element={Users}/>
-  //     <Route path='/Main' element={Main}/>
-  //   </Routes>
-
-  // </div>
-
 }
 
 export default App;
